e429593dd2d345bc8d561ca24aba608d72d6d07d
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-05 17:10:53 +0300

    Update support.js

--- commandgif/support.js
+++ commandgif/support.js
@@ -10,11 +10,17 @@ const embed = new Discord.MessageEmbed()
 .setColor(`RANDOM`)
 .setThumbnail(message.author.avatarURL({dynamic: "true"}))
 .setTitle(`**Links**`)
-.setDescription(`
-[Invite Me](https://discord.com/oauth2/authorize?client_id=806840212608909344&permissions=70346817&scope=bot)
--
-[Support Server](https://discord.gg/jcs4XwcExv)
-`)
+.setDescription(`<a:emoji_17:850611627321327617> OWNER BOT <@707980320355647519> <a:emoji_1:848805449155215391>
+<a:emoji_16:850611586921398283>
+<a:emoji_29:850653075055050772>[YOUTUBE](https://youtube.com/channel/UCU7jT6CLQvKIkyEzOuJFUPQ)
+<a:emoji_16:850611586921398283>
+<:g_:850633528697159710> [GITHUP](https://github.com/MAPXOR12)
+<a:emoji_16:850611586921398283>
+[SERVER SUPPORT](https://discord.gg/TTuqRTQrse)
+<a:emoji_16:850611586921398283>
+[SPONCAR SERVER](https://discord.gg/EpBZZYFre6)
+<a:emoji_16:850611586921398283>
+[INVITEBOT](https://discord.com/api/oauth2/authorize?client_id=815940940404359218&permissions=8&scope=bot)`)
 .setFooter(`Requested by ${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))    
 message.channel.send({embed});
 }
